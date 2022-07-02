2a90638031adaab1c11986c5a38fc63a32a5fa9b
Eman <emmanuelgatwech@gmail.com>
2022-07-02 17:17:15 +0300

    feat: setup db and prisma

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
+require("dotenv").config();
+const express = require("express");
+import * as bodyParser from "body-parser";
+import { Prisma, PrismaClient } from "@prisma/client";
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+connection.end();
