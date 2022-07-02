d1990182a59dbea1ef63f0fcccd2f51e03520459
Eman <emmanuelgatwech@gmail.com>
2022-07-02 14:21:07 +0300

    feat: edit button component

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,11 @@
-import * as React from "react";
-export const Button = () => {
+import { FC } from "react";
+
+type ButtonProps = {
+  onClick?: Function
+  primary?: boolean
+  fullWidth?: boolean
+}
+
+export const Button: FC<ButtonProps> = (props) => {
   return <button>Boop</button>;
 };
