883fafea711f412e3834617b607f06b321ab1591
Eman <emmanuelgatwech@gmail.com>
2022-07-08 11:44:52 +0300

    feat: add useSwr hook

--- apps/web/pages/profile.tsx
+++ apps/web/pages/profile.tsx
@@ -1,8 +1,6 @@
 import Layout from "../layout/layout";
 
 export default function Profile(props) {
-  console.log('oweee!');
-  console.log(props);
   return (
     <Layout>
       <h1>Profile</h1>
@@ -10,11 +8,13 @@ export default function Profile(props) {
   );
 }
 
-export function getStaticProps(context) {
-  console.log(context);
+export async function getStaticProps(context) {
+    // Fetch data from external API
+    const res = await fetch(`http://localhost:5000/api/users/1`)
+    const data = await res.json();
 
-  return {
-    props: {}
-  }
+    return {
+      props: {}, // will be passed to the page component as props
+    }
 
 }
