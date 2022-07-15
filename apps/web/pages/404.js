ae943e476cc3ce09d364aa37e7655982df5d8dfe
Righteous <ladulumori2@gmail.com>
2022-07-15 15:32:46 +0300

    Attempt to add an image from an external source

--- apps/web/pages/404.js
+++ apps/web/pages/404.js
@@ -1,6 +1,15 @@
+import { Image, Img } from '@chakra-ui/react'
+
 export default function Home() {
     return(
-    <div>
-    <p>"Sorry Mate!, Try some else : )"</p>
+    <div align="center">
+    {/* <p>"Sorry Mate!, Try some else : ) Like going back Home"</p> */}
+    <Image
+  borderRadius={'full'}
+  boxSize='500px'
+  width='1000px'
+  src='https://webdeasy.de/wp-content/uploads/2020/06/404-pages.jpg'
+  alt='Sorry Mate!, Try some else : )'
+/>
 </div>);
 }
