616f9f5ce4f34436b96fd8e7b07a41d510f16704
Eman <emmanuelgatwech@gmail.com>
2022-11-14 22:51:18 +0300

    chore: fix merge conflict

--- apps/web/components/landing/BackpackHighlights.tsx
+++ apps/web/components/landing/BackpackHighlights.tsx
@@ -7,7 +7,7 @@ import { RiSuitcase2Line } from "react-icons/ri";
 export const BackpackHighlights = () => {
     return (
         <VStack p="8" margin="0 auto">
-            <VStack flex="2" gap={8} textAlign="center">
+            <VStack flex="2" gap={4} textAlign="center">
                 <Box
                     boxShadow="rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"
                     p="4"
