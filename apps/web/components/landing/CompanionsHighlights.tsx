7de7da54a449e587306a948a92fe4bad28030a5e
Eman <emmanuelgatwech@gmail.com>
2022-11-14 20:45:29 +0300

    feat: add section copy

--- apps/web/components/landing/CompanionsHighlights.tsx
+++ apps/web/components/landing/CompanionsHighlights.tsx
@@ -1,38 +1,27 @@
 import { Box, Flex, Heading, VStack, Text } from "@chakra-ui/react";
 import Image from "next/image";
-import { FiMap } from "react-icons/fi";
+import { RiTeamLine } from "react-icons/ri";
 import { Card } from "ui";
 
 export const CompanionsHighlights = () => {
     return (
         <VStack p="8" margin="0 auto">
-            <VStack flex="2" gap={8} textAlign="center">
+            <VStack flex="2" gap={4} textAlign="center">
                 <Box
-                    boxShadow="0 0 0 1px rgba(0,0,0,0.1), 0 1px 2px 0 rgba(0,0,0,0.1)"
+                    boxShadow="rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"
                     p="4"
                     borderRadius="1rem"
+                    bg="brand.white"
+                    color="brand.secondary"
                 >
-                    <FiMap size="2rem" />
+                    <RiTeamLine size="2rem" />
                 </Box>
-                <Heading as="h1" size="2xl" >Choose your path</Heading>
-                <Text>
-                    Create your goal and break it down into objectives. Choose whether your journey is public or private. When your journey is public, other inspirers can see your goal and objectives, you can let them access your backpack or not!
+                <Heading as="h3" size="md" color="brand.secondary">Connect with peers</Heading>
+                <Heading size="lg" >Network with empowered folks who’re ready to go farther together</Heading>
+                <Text size="lg" color="brand.secondaryText">
+                    We’re in this together. Join our global community of like-minded peers. Discover and learn from those who share your aspirations, then stay connected to grow together. Collaborate in real-time using Inspirers Companion Mode! Inspirers is the platform you need for collectively achieving mind-blowing dreams.
                 </Text>
             </VStack>
-            <Flex gap={4}>
-                <Card>
-                    <Heading as="h1" size="md">Choose your path</Heading>
-                    <Text>
-                        Go on an amazing journey with Inspirers and get inspired by the people who are doing what you aspire to do. Advice, motivation, and encouragement are at your fingertips.
-                    </Text>
-                </Card>
-                <Card>
-                    <Heading as="h1" size="md">Choose your path</Heading>
-                    <Text>
-                        Go on an amazing journey with Inspirers and get inspired by the people who are doing what you aspire to do. Advice, motivation, and encouragement are at your fingertips.
-                    </Text>
-                </Card>
-            </Flex>
         </VStack>
     )
 }
--- apps/web/components/landing/index.tsx
+++ apps/web/components/landing/index.tsx
@@ -6,6 +6,8 @@ import { JourneyHighlights } from "./JourneyHighlights";
 import { Differentiators } from "./Differentiators";
 import { CompanionsHighlights } from "./CompanionsHighlights";
 import { BackpackHighlights } from "./BackpackHighlights";
+import { RoutinesSection } from "./RoutinesSection";
+import Routine from "pages/routines/[id]";
  
 export const Landing = () => {
   return (
@@ -14,6 +16,7 @@ export const Landing = () => {
       <JourneyHighlights />
       <CompanionsHighlights />
       <BackpackHighlights />
+      <RoutinesSection />
       <Differentiators />
       <InspirationPane />
       <InterestForm />
