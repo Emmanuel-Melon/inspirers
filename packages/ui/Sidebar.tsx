34044a0abd65a0115d73de201037315e68dda30f
Eman <emmanuelgatwech@gmail.com>
2023-01-14 23:45:40 +0300

    feat: add sidebar links as prps

--- packages/ui/Sidebar.tsx
+++ packages/ui/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactChild, useState } from "react";
+import { FC, ReactChild, useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import {
     Box,
@@ -12,27 +12,28 @@ import {
     MenuButton,
     MenuList,
     MenuItem,
-    IconButton
+    IconButton,
+    Drawer,
+    DrawerContent,
+    useDisclosure,
 } from "@chakra-ui/react";
 import { BeakerIcon } from '@heroicons/react/24/solid'
 import { Avatar, Card, Navbar, Spinner } from "ui";
 import Head from "next/head";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
+import { IconType } from 'react-icons';
 
-import {
-    FiHome,
-    FiSettings,
-    FiUser,
-    FiLogOut,
-    FiHelpCircle
-} from "react-icons/fi";
 
-import { RiApps2Line, RiHandbagLine, RiNotification3Line, RiRocket2Line } from "react-icons/ri";
+import { RiRocket2Line } from "react-icons/ri";
 
-import { HiOutlineBell, HiOutlineHome, HiOutlineTemplate, HiOutlineClipboardCheck, HiOutlineCollection } from "react-icons/hi";
 
-const SidebarLink = ({ link }) => {
+type SidebarLinkProps = {
+    name: string;
+    icon?: IconType;
+}
+
+const SidebarLink: FC<SidebarLinkProps> = ({ link }) => {
     return (
         <NextLink href={link.url} passHref>
             <LinkBox
@@ -71,56 +72,7 @@ const SidebarLink = ({ link }) => {
     )
 }
 
-export const Sidebar = ({ session }) => {
-    const links = [
-        {
-            id: 1,
-            name: "Home",
-            url: "/",
-            icon: <HiOutlineHome size="1rem" />
-        },
-        {
-            id: 4,
-            name: "Routines",
-            url: "/routines",
-            icon: <HiOutlineTemplate size="1rem" />
-        },
-        {
-            id: 5,
-            name: "Tasks",
-            url: "/tasks",
-            icon: <HiOutlineClipboardCheck size="1rem" />
-        },
-        {
-            id: 6,
-            name: "Notifications",
-            url: "/notifications",
-            icon: <RiNotification3Line size="1rem" />
-        },
-        {
-            id: 8,
-            name: "Backpack",
-            url: "/backpacks",
-            icon: <RiHandbagLine size="1rem" />
-        },
-        {
-            id: 9,
-            name: "Apps",
-            url: "/apps",
-            icon: <RiApps2Line size="1rem" />
-        }
-    ];
-
-    const bottomLinks = [
-        {
-            id: 7,
-            name: "Help",
-            url: "/support",
-            icon: <FiHelpCircle size="1rem" />
-        }
-    ]
-
-    const router = useRouter();
+export const Sidebar = ({ links }) => {
     return (
         <Stack
             p={8}
@@ -160,65 +112,6 @@ export const Sidebar = ({ session }) => {
                         links.map(link => <SidebarLink key={link.id} link={link} />)
                     }
                 </Stack>
-            </Stack>
-
-            <Stack gap={2}>
-                <Stack gap={2}>
-                    {
-                        bottomLinks.map(link => <SidebarLink key={link.id} link={link} />)
-                    }
-                </Stack>
-                <Flex borderRadius="1rem" gap={4} alignItems="center">
-                    <Menu >
-                        <MenuButton
-                            as={IconButton}
-                            aria-label='Options'
-                            icon={<FiUser />}
-                            variant='outline'
-                            width="fit-content"
-                            bg="brand.white"
-                            size="sm"
-                            borderRadius="50%"
-                            p="2"
-                            boxShadow="rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"
-                        />
-                        <MenuList>
-                            <MenuItem
-                                icon={<FiUser />}
-                                onClick={() => router.push(`/user/${session?.user?.id}`)}
-                                _hover={{
-                                    bg: "brand.highlight3",
-                                }}
-                                _active={{
-                                    bg: "brand.highlight3",
-                                }}
-                            >
-                                Profile
-                            </MenuItem>
-                            <MenuItem
-                                icon={<FiSettings />}
-                                onClick={() => router.push(`/settings`)}
-                                _hover={{
-                                    bg: "brand.highlight3",
-                                }}
-                            >
-                                Settings
-                            </MenuItem>
-                            <MenuItem
-                                icon={<FiLogOut />}
-                                onClick={() => signOut()}
-                                _hover={{
-                                    bg: "brand.highlight3",
-                                }}
-                            >
-                                Logout
-                            </MenuItem>
-                        </MenuList>
-                    </Menu>
-                    <Stack>
-                        <Text size="xs">@junubiman</Text>
-                    </Stack>
-                </Flex>
             </Stack>
         </Stack>
     )
