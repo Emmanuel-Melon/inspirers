e405959b6ddc24b94111e6894e17c84291893020
Eman <emmanuelgatwech@gmail.com>
2023-02-16 18:08:44 +0300

    chore: refactor pages

--- apps/web/pages/routines/index.tsx
+++ apps/web/pages/routines/index.tsx
@@ -9,20 +9,29 @@ import {
   StatLabel,
   StatNumber,
   StatHelpText,
+  Tag,
 } from "@chakra-ui/react";
-import { useFetch } from "../../hooks/useSwr";
-import { ListRoutines } from "../../core/Routines/ListRoutines";
-import { AddRoutine } from "../../core/Routines/AddRoutine";
+import { useFetch } from "hooks/useSwr";
+import { ListRoutines } from "core/Routines/ListRoutines";
+import { ActiveRoutine } from "core/Routines/ActiveRoutine";
+import { AddRoutineModal } from "core/Routines/AddRoutineModal";
 import { client } from "utils/client";
+import { HiOutlineHome, HiOutlineTemplate } from "react-icons/hi";
+import NextLink from "next/link";
 
-import { Button, Card, Modal, IconButton, LayoutController } from "ui";
-import { unstable_getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]";
-import { GetServerSidePropsContext } from "next";
+import { Avatar, Button, Card, Modal, IconButton, LayoutController } from "ui";
 import { JourneyContext } from "providers/JourneyProvider";
-import { FiList, FiSettings, FiCalendar, FiGrid } from "react-icons/fi";
+import {
+  FiList,
+  FiSettings,
+  FiCalendar,
+  FiGrid,
+  FiStar,
+  FiZap,
+} from "react-icons/fi";
 import { useSession } from "next-auth/react";
 import toast, { Toaster } from "react-hot-toast";
+import { Timeline } from "core/Routines/TimelineView";
 
 // infer SSR Props!
 const layouts = [
@@ -47,36 +56,18 @@ const layouts = [
 ];
 
 export default function RoutinesPage(props) {
+  const { data: session } = useSession();
   const context = useContext(JourneyContext);
   const {
     data: routines,
     isLoading,
     isError,
-  } = useFetch(`/routines/${context.journey.userId}/list`);
+  } = useFetch(`/routines/${session?.user?.id}/list`);
   const [view, setView] = useState("list");
-  const errorToast = (message: string) => toast.error(message);
-  const successToast = (message: string) => toast.success(message);
 
 
   const changeView = (view: string): void => {
     setView(view);
-  };
-
-
-  const { data: session } = useSession();
-
-  const addNewRoutine = (data) => {
-    client.post(`/routines`, {
-      ...data,
-      userId: context.journey.userId
-    })
-    .then(res => {
-      console.log(res);
-      successToast("Routine created");
-    }).catch(err => {
-      console.log(err);
-      errorToast(err.message);
-    })
   };
 
   return (
@@ -85,83 +76,121 @@ export default function RoutinesPage(props) {
         <Flex justifyContent="space-between" alignItems="center" gap={8}>
           <Stack flex="2">
             <Heading size="md">Routines</Heading>
-            <Text>
-              The key to managing your time is performing the right habits
-              everyday. These habits will improve your life and help you
-              optimize it to reach your goals.
-            </Text>
           </Stack>
-          <AddRoutine  addNewRoutine={addNewRoutine} />
+          <AddRoutineModal />
         </Flex>
-        <LayoutController layouts={layouts} changeView={changeView} />
-        <ListRoutines
-          routines={routines?.data}
-          isLoading={isLoading}
-          isError={isError}
-        />
         <Flex justifyContent="space-between" gap={4}>
-          <Box flex="2">
+          <Stack flex="1">
+            <Card bg="brand.primary">
+              <Stack>
+                <Box
+                  p="2"
+                  bg="brand.white"
+                  width="fit-content"
+                  borderRadius="0.5rem"
+                >
+                  <HiOutlineTemplate size="1.5rem" />
+                </Box>
+                <NextLink href={`/user/${session?.user?.id}`}>
+                <Heading size="md" color="brand.primaryText">
+                  Hi, {session?.user?.name}
+                </Heading>
+                </NextLink>
+                <Text color="brand.secondaryText">
+                  The key to managing your time is performing the right habits
+                  everyday. These habits will improve your life and help you
+                  optimize it to reach your goals.
+                </Text>
+              </Stack>
+            </Card>
+            <Flex gap={4}>
+              <Card bg="brand.white">
+                <Stat>
+                  <StatLabel color="brand.secondaryText">
+                    Daily Streak
+                  </StatLabel>
+                  <StatNumber color="brand.accent">3 days</StatNumber>
+                  <StatHelpText>Feb 01 - Feb 28</StatHelpText>
+                </Stat>
+              </Card>
+            </Flex>
+            <Flex gap={4}>
+              <Card bg="brand.white">
+                <Stat>
+                  <StatLabel color="brand.secondaryText">
+                    Objectives Completed
+                  </StatLabel>
+                  <StatNumber color="brand.accent">1</StatNumber>
+                  <StatHelpText>Feb 01 - Feb 28</StatHelpText>
+                </Stat>
+              </Card>
+              <Card bg="brand.white">
+                <Stat>
+                  <StatLabel color="brand.secondaryText">Hours spent</StatLabel>
+                  <StatNumber color="brand.accent">2 hours</StatNumber>
+                  <StatHelpText color="brand.secondaryText">
+                    Feb 01 - Feb 28
+                  </StatHelpText>
+                </Stat>
+              </Card>
+            </Flex>
+          </Stack>
+          <Stack flex="2">
+            <Card>
+              <Stack>
+                <Text>My Schedule</Text>
+                <Text>Show active hours</Text>
+                <Text>Show bar or something</Text>
+              </Stack>
+            </Card>
+            <LayoutController layouts={layouts} changeView={changeView} />
             <ListRoutines
               routines={routines}
               isLoading={isLoading}
               isError={isError}
             />
-          </Box>
+          </Stack>
           <Stack flex="1">
-            <Card bg="brand.highlight1">
-              <Stack gap={2}>
-                <Flex justifyContent="space-between" alignItems="center">
-                  <Heading size="xs">Mid-Day Routine</Heading>
-                  <IconButton
-                    label={""}
-                    onClick={function (): void {}}
-                    color="brand.secondary"
-                  >
-                    <FiSettings />
-                  </IconButton>
+            <Flex justifyContent="space-between">
+              <Flex gap={2} alignItems="center">
+                <FiZap />
+                <Heading size="sm">Automations</Heading>
+              </Flex>
+              <NextLink href="routines/automations">View All</NextLink>
+            </Flex>
+            <Card bg="brand.white">
+              <Stack>
+                <Flex gap={2} alignItems="center" color="brand.accent">
+                  <FiZap />
+                  <Heading size="sm">Zen Mode</Heading>
                 </Flex>
-                <Flex gap={4}>
-                  <Card>
-                    <Stat>
-                      <StatLabel color="brand.secondaryText">
-                        Activity
-                      </StatLabel>
-                      <StatNumber color="brand.secondary">2 days</StatNumber>
-                      <StatHelpText>Sep 20 - Present</StatHelpText>
-                    </Stat>
-                  </Card>
+                <Text>Closes all tabs.</Text>
+                <Tag
+                  boxShadow="rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"
+                  color="brand.secondaryText"
+                >
+                  When Routine Starts
+                </Tag>
+              </Stack>
+            </Card>
+            <Card bg="brand.white">
+              <Stack>
+                <Flex gap={2} alignItems="center" color="brand.accent">
+                  <FiZap />
+                  <Heading size="sm">Lofi</Heading>
                 </Flex>
-                <Heading size="xs">Overview</Heading>
-                <Flex gap={4}>
-                  <Card>
-                    <Stat>
-                      <StatLabel color="brand.secondaryText">
-                        Articles
-                      </StatLabel>
-                      <StatNumber color="brand.secondary">13</StatNumber>
-                    </Stat>
-                  </Card>
-                  <Card>
-                    <Stat>
-                      <StatLabel color="brand.secondaryText">Videos</StatLabel>
-                      <StatNumber color="brand.secondary">13</StatNumber>
-                    </Stat>
-                  </Card>
-                  <Card>
-                    <Stat>
-                      <StatLabel color="brand.secondaryText">
-                        Challenges
-                      </StatLabel>
-                      <StatNumber color="brand.secondary">13</StatNumber>
-                    </Stat>
-                  </Card>
-                </Flex>
+                <Text>Closes all tabs.</Text>
+                <Tag
+                  boxShadow="rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"
+                  color="brand.secondaryText"
+                >
+                  When Routine Starts
+                </Tag>
               </Stack>
             </Card>
           </Stack>
         </Flex>
       </Stack>
-      <Toaster position="bottom-center" />
     </>
   );
 }
